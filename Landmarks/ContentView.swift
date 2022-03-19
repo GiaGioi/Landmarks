bf504d0ef1c70d89afe694658d3fed487af9fe19
GiaGioi99 <gioing@funtap.vn>
2022-03-19 22:42:59 +0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Apple on 1/3/22.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text ("Hello Swift")
+            .padding()
+            .foregroundColor(.green)
+            
+    }
+}
+
+
+struct ContentView_Previews : PreviewProvider {
+    static var previews : some View{
+            ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Apple on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
